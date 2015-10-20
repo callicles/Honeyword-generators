e46bfe4a34ddf429a868b6e44961b35a7fe4d223
Rishav Kanoria <Rishav@dhcp-ccc-322.eduroam.cornell.edu>
2015-10-20 16:42:17 -0400

    updated classifier

--- classifier.py
+++ classifier.py
@@ -1,7 +1,7 @@
 import enchant
 
-def classifier(password):
-	""" Classifies the given password into one of several categories.
+def classifier(token):
+	""" Classifies the given token into one of several categories.
 	categories currently defined are:
 		letter (single alphabet),
 		digit (single number),
@@ -19,40 +19,40 @@ def classifier(password):
 	USEnglishDict = enchant.Dict("en_US")
 	GBEnglishDict = enchant.Dict("en_GB")
 
-	#check if password is a single character
-	if len(password) == 1:
+	#check if token is a single character
+	if len(token) == 1:
 		#check if is an alphabet
-		if password.isalpha():
+		if token.isalpha():
 			return "Letter"
 		#check if is a digit
-		elif password.isdigit():
+		elif token.isdigit():
 			return "Digit"
 		else:
 			return "SpecialChar"
 
-	#password is more than 1 character
+	#token is more than 1 character
 	else:
-		#check if password is a string of alphabets, check if a dictionary word or not
-		if password.isalpha():
-			if USEnglishDict.check(password) or GBEnglishDict.check(password):
+		#check if token is a string of alphabets, check if a dictionary word or not
+		if token.isalpha():
+			if USEnglishDict.check(token) or GBEnglishDict.check(token):
 				return "DictionaryWord"
 			else:
 				return "RandomWord"
-		#check if password is a number, sort between even and odd
-		elif password.isdigit():
+		#check if token is a number, sort between even and odd
+		elif token.isdigit():
 			evenCount = 0
 			oddCount = 0
-			for x in range(len(password)):
-				if password[x]%2 ==0:
+			for x in range(len(token)):
+				if token[x]%2 ==0:
 					evenCount+=1
 				else:
 					oddCount+=1
-			if evenCount == len(password):
+			if evenCount == len(token):
 				return "AllEvenDigits"
-			elif oddCount == len(password):
+			elif oddCount == len(token):
 				return "AllOddDigits"
 			else:
-				if password%2 == 0:
+				if token%2 == 0:
 					return "EvenNumber"
 				else:
 					return "OddNumber"
