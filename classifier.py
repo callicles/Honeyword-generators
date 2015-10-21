cc5d9b9bc2a5a5b9a0a6195acddc4e5a8193ae5b
Mark <mark@dhcp-ccc-1462.eduroam.cornell.edu>
2015-10-21 19:57:18 -0400

    fixing indents

--- classifier.py
+++ classifier.py
@@ -1,28 +1,28 @@
 from constants import ENGLISH_INDEX
 
 def isInAlphabeticalSequence(word):
-	""" 
-	Checks if the string passed to it is in an alphabetical sequence
-	"""
-	if len(word) == 1:
-		return False
-	else:
-		for i in range(len(word) - 1):
-			if word[i] != word[i + 1] + 1:
-            	return False
-    	return True
+    """
+    Checks if the string passed to it is in an alphabetical sequence
+    """
+    if len(word) == 1:
+        return False
+    else:
+        for i in range(len(word) - 1):
+            if word[i] != word[i + 1] + 1:
+                return False
+        return True
 
 def isInReverseAlphabeticalSequence(word):
-	"""
-	Checks if the string passed to it is in a reverse alphabetical sequence
-	"""
-	if len(word) == 1:
-		return False
-	else:	
-		for i in range(len(word) - 1):
-			if word[i] != word[i + 1] - 1:
-				return False
-	    return True
+    """
+    Checks if the string passed to it is in a reverse alphabetical sequence
+    """
+    if len(word) == 1:
+        return False
+    else:
+        for i in range(len(word) - 1):
+            if word[i] != word[i + 1] - 1:
+                return False
+        return True
 
 def isSameCharacterSequence(word):
 	"""
@@ -70,57 +70,60 @@ def classifyCharacter(word):
 		return "isspecialchar"
 
 def classifier(token):
-	""" Classifies the given token into one of several categories.
-	categories currently defined are:
-		same_sequence_letters: same letter being repeated (e.g. "aaa")
-		sequence_letters: letters in alphabetical sequence or reverse alphabetical sequence (e.g. "abc", "zyx")
-		random_letters: string of random letters (e.g. dyumd)
-		same_sequence_numbers: same digit being repeated (e.g. "22222")
-		sequence_numbers: logical sequence of digits (e.g. "369")
-		even_numbers: all digits are even digits (e.g. "4824")
-		odd_numbers: all digits are odd digits (e.g. "35573")
+
+    """ Classifies the given token into one of several categories.
+    categories currently defined are:
+        same_sequence_letters: same letter being repeated (e.g. "aaa")
+        sequence_letters: letters in alphabetical sequence or reverse alphabetical sequence (e.g. "abc", "zyx")
+        random_letters: string of random letters (e.g. dyumd)
+        same_sequence_numbers: same digit being repeated (e.g. "22222")
+        sequence_numbers: logical sequence of digits (e.g. "369")
+        even_numbers: all digits are even digits (e.g. "4824")
+        odd_numbers: all digits are odd digits (e.g. "35573")
 
 
-	Classifies by priority:
-	words: constant sequence > sequence > random
-	numbers: constant sequence > sequence > odd or even > random numbers
-	special chars: constant sequence > random
-	"""
+    Classifies by priority:
+    words: constant sequence > sequence > random
+    numbers: constant sequence > sequence > odd or even > random numbers
+    special chars: constant sequence > random
+    """
 
-	#check if token is a string of alphabets
-		if token.isalpha():
-			#check if token is a sequence of the same alphabet:
-			if isSameCharacterSequence(token):
-				return "same_sequence_letters"
-			elif isInAlphabeticalSequence(token) or isInReverseAlphabeticalSequence(token):
-				return "sequence_letters"
-			else:
-				return "random_letters"
-		
-		#check if token is a number
-		elif token.isdigit():
-			if isSameCharacterSequence(token):
-				return "same_sequence_numbers"
-			elif isInSequence(token):
-				return "sequence_numbers"
-			else:
-				evenCount = 0
-				oddCount = 0
-				for x in range(len(token)):
-					if int(token[x])%2 ==0:
-						evenCount+=1
-					else:
-						oddCount+=1
-				if evenCount == len(token):
-					return "even_numbers"
-				elif oddCount == len(token):
-					return "odd_numbers"
-				else:
-					return "random_numbers"
-		else:
-			if isSameCharacterSequence(token):
-				return "same_sequence_specialchars"
-			else:
-				return "random_specialchars"
+    #check if token is a string of alphabets
+
+
+    if token.isalpha():
+        #check if token is a sequence of the same alphabet:
+        if isSameCharacterSequence(token):
+            return "same_sequence_letters"
+        elif isInAlphabeticalSequence(token) or isInReverseAlphabeticalSequence(token):
+            return "sequence_letters"
+        else:
+            return "random_letters"
+
+    #check if token is a number
+    elif token.isdigit():
+        if isSameCharacterSequence(token):
+            return "same_sequence_numbers"
+        elif isInSequence(token):
+            return "sequence_numbers"
+        else:
+            evenCount = 0
+            oddCount = 0
+            for x in range(len(token)):
+                if int(token[x])%2 ==0:
+                    evenCount+=1
+                else:
+                    oddCount+=1
+            if evenCount == len(token):
+                return "even_numbers"
+            elif oddCount == len(token):
+                return "odd_numbers"
+            else:
+                return "random_numbers"
+    else:
+        if isSameCharacterSequence(token):
+            return "same_sequence_specialchars"
+        else:
+            return "random_specialchars"
 
 
